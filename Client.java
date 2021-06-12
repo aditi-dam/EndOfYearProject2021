64c0b5bec47260e49f25174276f2d2549268c877
aaacoder <aditi.dam@gmail.com>
2021-06-12 11:26:03 -0400

    aaaagh?

--- Client.java
+++ Client.java
@@ -117,11 +117,6 @@ public class Client extends Application {
                     System.out.println("Have fun!");
                 }
                 else if (line.toLowerCase().equals("/pictionary")) {
-<<<<<<< HEAD
-                    if (playerNum == -1){
-                        playerNum = 1;
-                        out.println("START");
-=======
                     String word = "";
                     try{
                         if (playerNum == -1){
@@ -136,23 +131,11 @@ public class Client extends Application {
                             System.out.println("YOU SAID " + line);
                             System.out.println("CORRECT " + word);
                         }
->>>>>>> bb43a7db956694878d99c628d5828c8d50ed20d3
                     }
                     catch (IOException e) {
                         e.printStackTrace();
                     }     
                 }
-<<<<<<< HEAD
-                if (playerNum == 1){
-                    try {
-                        int randomWord = (int) (Math.random() * "pictionary_idea.txt".length());
-                        String word = Files.readAllLines(Paths.get("pictionary_ideas.txt")).get(randomWord);
-                        System.out.println(word);
-                        // https://stackoverflow.com/questions/2312756/how-to-read-a-specific-line-using-the-specific-line-number-from-a-file-in-java
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }                
-=======
                 //https://stackoverflow.com/questions/2312756/how-to-read-a-specific-line-using-the-specific-line-number-from-a-file-in-java
                  
                 else if(playerNum % 2 == 0){
@@ -162,7 +145,6 @@ public class Client extends Application {
                     else{
                         System.out.println("KEEP TRYING");
                     }
->>>>>>> bb43a7db956694878d99c628d5828c8d50ed20d3
                 }
                 
                 out.println(line);
