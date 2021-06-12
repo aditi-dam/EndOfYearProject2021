f90f2c066ca0084329232a50da487997b9f2a06a
Satwika <satwika.vemuri@gmail.com>
2021-06-12 08:19:22 -0400

    word is saved in receiving client

--- Client.java
+++ Client.java
@@ -1,8 +1,15 @@
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.nio.file.Paths;
 import java.util.Scanner;
+import java.util.stream.Stream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 
 import javafx.application.Application;
 import javafx.scene.Scene;
@@ -27,7 +34,7 @@ public class Client extends Application {
     public static StackPane pane = new StackPane();
     public static Scene scene = new Scene(pane, 800, 500);
     public static Scanner userInput = new Scanner(System.in);
-
+    
     private static int playerNum = -1;
 
     public static void main(String[] args) throws Exception {
@@ -81,6 +88,7 @@ public class Client extends Application {
                     }
                     else if(incoming.startsWith("START")){
                         playerNum = 2;
+                        Whiteboard.setWord(incoming.substring(incoming.indexOf("T", 3) + 1));
                     }
 
                 }
@@ -109,25 +117,42 @@ public class Client extends Application {
                     System.out.println("Have fun!");
                 }
                 else if (line.toLowerCase().equals("/pictionary")) {
-                    if(playerNum == -1){
-                        playerNum = 1;
-                        out.println("START");
+                    String word = "";
+                    try{
+                        if (playerNum == -1){
+                            playerNum = 1;
+                            int randomWord = (int) (Math.random() * "pictionary_idea.txt".length());
+                            word = Files.readAllLines(Paths.get("pictionary_ideas.txt")).get(randomWord);
+                            System.out.println(word);
+                            Whiteboard.setWord(word);
+                            out.println("START" + word);
+                        }
+                        else if(playerNum == 2){
+                            System.out.println("YOU SAID " + line);
+                            System.out.println("CORRECT " + word);
+                        }
                     }
+                    catch (IOException e) {
+                        e.printStackTrace();
+                    }     
                 }
-                if(playerNum == 1){
-                    //SOMEONE: do pick random word and print it here
+                //https://stackoverflow.com/questions/2312756/how-to-read-a-specific-line-using-the-specific-line-number-from-a-file-in-java
+                 
+                else if(playerNum % 2 == 0){
+                    System.out.println("YOU SAID: " + line);
+                    System.out.println("WORD IS: " + Whiteboard.getWord());
                 }
                 
                 out.println(line);
                 line = userInput.nextLine().trim();
 
             }
-            try{ 
-            out.println("QUIT");
-            out.close();
-            userInput.close();
-            socketIn.close();
-            socket.close();
+            try { 
+                out.println("QUIT");
+                out.close();
+                userInput.close();
+                socketIn.close();
+                socket.close();
             }
             catch(Exception e){};
         }
