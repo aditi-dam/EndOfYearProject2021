a5d17dd46a7b4c279c3c861d1e8ec59755767110
Aparna <aparnarenjithpillai@gmail.com>
2021-06-15 18:44:06 -0400

    dafeszdA wegfawreg

--- Whiteboard.java
+++ Whiteboard.java
@@ -43,6 +43,7 @@ public class Whiteboard extends Application{
     private Text guessCount = new Text();
     private Text username = new Text();
     private Stage ps;
+    private Label label1;
 
 
     public static String getWord() {
@@ -154,21 +155,14 @@ public class Whiteboard extends Application{
                 
 
                 HBox h = new HBox(10);
-
-
-                HBox h1 = new HBox();
-
-                HBox h2 = new HBox();
              
                 h.getChildren().add(tf);
-                h1.getChildren().add(submit);
-                h2.getChildren().add(guessCount);
+                h.getChildren().add(submit);
+                h.getChildren().add(guessCount);
+                h.getChildren().addAll(tf,submit,guessCount,label1);
                 h.setAlignment(Pos.BOTTOM_LEFT);
-                h1.setAlignment(Pos.BOTTOM_LEFT);
-                h2.setAlignment(Pos.TOP_RIGHT);
+
                 h.setPadding(new Insets(5,5,5,5));
-                h1.setPadding(new Insets(5,5,5,5));
-                h2.setPadding(new Insets(5,5,5,5));
 
 
                 submit.setAlignment(Pos.CENTER_RIGHT);
@@ -194,7 +188,7 @@ public class Whiteboard extends Application{
         Platform.runLater(() ->{ 
                         
             if(!(getWord().equals(tf.getText())) && (guesses < 7)){
-                guessCount.setText("Keep Trying! Guesses: " + (++guesses));
+                label1.setText("Keep Trying! Guesses: " + (++guesses));
             }
             else if(guesses >= 7){
                 out.println("WON");
